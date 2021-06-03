4e86eb0ec34d393a58656521f527fe69ad9e96b5
ddb1494 <35029624+ddb1494@users.noreply.github.com>
2021-06-03 14:49:01 +0900

    d20210603

---
+++ watchfile/index.js
@@ -0,0 +1,26 @@
+let socket = io()
+
+socket.on('connect', function(){
+	console.log('connectd')
+})
+
+socket.on('close', function(){
+	console.log('closed')
+})
+
+
+
+
+let vm = new Vue({
+	el:'#app',
+	data: {
+		filename:'',
+		url:'',
+		ws: [],
+	},
+	methods: {
+		unwatch(event){
+
+		}
+	}
+})
